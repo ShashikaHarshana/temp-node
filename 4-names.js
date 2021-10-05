c552ebf49b517715d29df6445104731e2836834b
ShashikaHarshana <88888333+ShashikaHarshana@users.noreply.github.com>
2021-10-05 15:40:19 +0530

    first commit

---
+++ 4-names.js
@@ -0,0 +1,5 @@
+const secret = 'Super SEcreat'
+const peter = 'peter'
+const susan = 'susan'
+
+module.exports = { peter, susan }
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,6 @@
+module.exports.items = ['item1', 'item2']
+const person = {
+  person: 'Bob'
+}
+
+module.exports.singlePerson = person
